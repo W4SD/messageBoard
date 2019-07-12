03512298127aa51b2428d58a0af5a8e4815c7a76
Antti <antti.oksman101@gmail.com>
2019-07-12 15:25:42 +0300

    CreateMessage form works

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
+from formLogic import CreateMessage
 
 app = Flask(__name__,
             template_folder="templates")
 
 @app.route("/")
 def home():
-    """Serve homepage template."""
-    return render_template("board.html")
+    """Serve message form."""
+    sendMessageForm = CreateMessage()
+    return render_template("messageForm.html", form=sendMessageForm)
 
 if __name__ == '__main__':
    app.run(debug = True)
--- formLogic.py
+++ formLogic.py
@@ -1,20 +1,20 @@
 from wtforms import Form, StringField, validators, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Length
 
-class SubmitMessage(Form):
+class CreateMessage(Form):
         """Send message to the board Form"""
 
-        sender = StringField('Name', [
+        sender = StringField('Name: ', [
             Length(min=4, max=10, message=('Name should be 4-10 characters long')),
             DataRequired(message=('Choose a viable name!'))
             ])
 
-        title = StringField('Title', [
+        title = StringField('Title: ', [
             Length(min=1, max=20, message=('A message should have title between 1-20 characters')),
             DataRequired(message=('Choose a good title'))
         ])
 
-        content = TextAreaField('Content', [
+        content = TextAreaField('Content: ', [
             Length(min=1, max=128, message=('Content can be max 128 characters!')),
             DataRequired(message=('What say u?'))
         ])
